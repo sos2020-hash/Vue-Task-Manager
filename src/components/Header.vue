fb029b062935a0f183e68a02e50dd06b254a375a
sos2020-hash <qweiop525@gmail.com>
2021-11-10 21:09:42 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button 
+        @btn-click="$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close' : 'Show'" 
+        :color="showAddTask ? 'red': 'green'"
+        :showAddTask="showAddTask"
+        />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    showAddTask: Boolean
+  },
+  components: {
+    Button,
+  }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
